0b99de6ad564a9e3a23d10f678693ca18ee9db2d
unknown <sagar22091998@gmail.com>
2020-02-08 00:33:49 +0530

    Nothing

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -5,21 +5,12 @@ import { logoutProfile } from './userFunctions'
 class Nav extends Component{
 
 
-  constructor(props){
-    super(props);
-    this.state={
-      sissoko : (localStorage.authToken===undefined)
-    };
-    this.logout  = this.logout.bind(this); 
-  }
-
   logout(){
     const token = localStorage.authToken
     logoutProfile(token).then(res => {
       localStorage.removeItem('authToken');
-      this.setState({sissoko:false});
     })
-
+    
 
   }
 
@@ -32,11 +23,11 @@ class Nav extends Component{
           <ul>
               <NavLink exact activeClassName="Current" className="Nav-Links" to="/">Home</NavLink>
               <NavLink  activeClassName="Current"  className="Nav-Links" to="/about">About</NavLink>
-              {this.state.sissoko ?
+              {localStorage.authToken===undefined?
               <NavLink  activeClassName="Current" className="Nav-Links" to="/register/login">Register | Login</NavLink>
               :
               <NavLink  activeClassName="Current" className="Nav-Links" to="/profile">Profile</NavLink>}
-              {!this.state.sissoko  ?
+              {localStorage.authToken!==undefined ?
               <NavLink   className="Nav-Links" to="/" onClick={this.logout}>Logout</NavLink>:null
               }
           </ul>
