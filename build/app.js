64f2311eec4e485a0be701a97646006a9d4607f0
ruturajkanake <ruturajkanake1999@gmail.com>
2021-01-04 22:54:00 +0530

    feat: :wrench: added localhost to cors policy

--- build/app.js
+++ build/app.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = require("dotenv");
 const body_parser_1 = __importDefault(require("body-parser"));
 const connect_1 = require("./config/connect");
@@ -14,6 +15,19 @@ const app = express_1.default();
 app.use(morgan_1.default('dev'));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
+const allowedOrigins = ['http://localhost:3000'];
+app.use(cors_1.default({
+    origin(origin, callback) {
+        if (!origin)
+            return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 app.get('/', (req, res) => {
     res.status(200).json('Welcome to Eduspark!');
 });
--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { config } from 'dotenv';
 import bodyParser from 'body-parser';
 import { connectFunc } from './config/connect';
@@ -12,6 +13,23 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const allowedOrigins: string[] = ['http://localhost:3000'];
+
+app.use(
+    cors({
+        origin(origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                const msg =
+                    'The CORS policy for this site does not ' +
+                    'allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
+    })
+);
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json('Welcome to Eduspark!');
